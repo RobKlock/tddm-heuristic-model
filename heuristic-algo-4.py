c6b11ead30fc9077e35ccd8637d378f5f72a6780
RobKlock <rklock@oberlin.edu>
2022-04-05 14:15:16 -0500

    responding seems to work right

--- heuristic-algo-4.py
+++ heuristic-algo-4.py
@@ -153,14 +153,60 @@ def coin_flip_update_rule(timer_values, timer, timer_indices, start_time, end_ti
                     timer_weight = lateUpdateRule(value, timer.timerWeight(idx), timer.learningRate(idx))
                     timer.setTimerWeight(timer_weight, idx)
 
+def respond(timer_value, event_time, next_event, ax1, idx):
+    start_threshold_times = start_threshold_time(timer_value, next_event)
+    start_threshold_times.sort()
+    start_threshold_times = np.vstack((start_threshold_times, np.ones(len(start_threshold_times)))).T
     
+    stop_threshold_times = stop_threshold_time(timer_value, next_event)
+    stop_threshold_times.sort()
+    stop_threshold_times = np.vstack((stop_threshold_times, (-1* np.ones(len(stop_threshold_times))))).T
+    
+    start_stop_pairs = np.vstack((start_threshold_times, stop_threshold_times))
+    start_stop_pairs = start_stop_pairs[start_stop_pairs[:, 0].argsort()]
+
+    responses = []
+    response_periods = []
+    k = 0
+    k_o = 0 # old value of k
+    # print(event_time)
+    # print(start_stop_pairs)
+    # Form list of start and stop events, sorted by time (a1, sig1, a2, a3, sig2, sig3 etc)
+    # Loop through all, if start event, k++, else, k--
+    # Identify all periods of k > K
+    # Fill with Poisson seq (samples then add the start time to all of them)
+    # once theyre greater than the boundary where they stop, throw them out
+    
+    for jdx, time in enumerate(start_stop_pairs):
+        k+=time[1]
+        # print(f'k: {k} \t time: {time[0]}')
+        # We're entering a response period
+        if k_o < K and k >= K:
+            response_period_start = time[0]
+        if k_o >=K and k < K:
+            response_period_end = time[0]
+            response_periods.append([response_period_start, response_period_end])
+        k_o+=time[1]
+    r = list(generate_responses(next_event-event_time))
+    r.insert(0, event_time)
+    r=list(np.cumsum(r))
+    # print(response_periods)
+    # print(r)
+    
+    for response_period in response_periods:
+        responses.extend([i for i in r if (i>response_period[0] and i<response_period[1] and i<next_event and i>event_time)])
+    
+    ax1.plot(responses, np.ones(len(responses)), 'x') 
+    return responses
+
+
 # A ramp has a start-event s_1 (last reset), weight w (inf)
 # A timer has start-event s_1, weight w, and stop event s_2, can be off
 # Universal start threshold = 0.9, stop = 1.1. Timer threshold is 1
 
 dt = 0.1
 N_EVENT_TYPES= 2 # Number of event types (think, stimulus A, stimulus B, ...)
-NUM_EVENTS=1 # Total amount of events
+NUM_EVENTS=10# Total amount of events
 Y_LIM=2 # Vertical plotting limit
 NOISE=0.04 # Internal noise - timer activation
 LEARNING_RATE=.99 # Default learning rate for timers
@@ -179,9 +225,7 @@ HOUSE_LIGHT_ON = [*range(0,NUM_EVENTS+1,1)]
 #events_with_type = TM.getSamples(NUM_EVENTS, num_normal = N_EVENT_TYPES)
 events_with_type = TM.getSamples(NUM_EVENTS, num_normal = N_EVENT_TYPES, scale_beg = 20, scale_end = 30)
 events_with_type = np.insert(events_with_type, 0, [0,0,0], axis=0)
-NUM_EVENTS = NUM_EVENTS+1
-
-event_occurances = (list(zip(*events_with_type))[0]) # Relative occurance of event
+NUM_EVENTS += 1
 
 events = np.zeros(NUM_EVENTS)
 error_arr = np.zeros(NUM_EVENTS)
@@ -193,48 +237,38 @@ for i in range (1,NUM_EVENTS):
 # Time axis for plotting        
 T = events_with_type[-1][0]
 
-# Timer with x ramps, all initialized to be very highly weighted (n=1)
+# Timer with N ramps, all initialized to be very highly weighted (n=1)
 timer=TM(1,200)
 
 ax1 = plt.subplot(211)
 ax2 = plt.subplot(212)
 
-timer.eventDict()[0] = np.arange(0,10).tolist() # Initialize ten ramps to first event type
-free_indices = np.arange(10,200) # Establish free ramps
+#timer.eventDict()[0] = np.arange(0,10).tolist() # Initialize ten ramps to first event type
+free_indices = np.arange(0,200) # Establish free ramps
 
 # Timers are allocated to an event type based on the timer's eventDict object
 # eventDict takes in an event type as a key and gives an array of timer indices 
 # for that object as the value
 first_event = True
 
-# At each event e_i
-# From all not idle ramps
 # Ramps with terminating event s_2 = e_i or s_2 is unassigned 
 # Random collection of N ramps are updated for interval s1 -> s2 = e_i
 
-# Active ramps (act>stop) are reset and s_1 is set to e_i
-# K idle ramps are started
-
-# If R ramps are between start and stop thresholds, respond
-for idx, event in enumerate(events_with_type):
+# At each event e_i
+for idx, event in enumerate(events_with_type[:-1]):    
     # Two cases: 
         # First Event
             # Start at zero and look at the next event
         # Middle Event
-            # Start at prior event and look at next event
-        
-        
+            # Start at prior event and look at next event   
     house_light = True
-    prev_event = 0
+    first_event=False  
     event_time = event[0]
     event_type = int(event[1])
     stimulus_type = int(event[2])
-    
-    try:
-        next_stimulus_type = int(events_with_type[idx+1][2])
-    except IndexError:
-        next_stimulus_type = -1
-    
+    next_event = events_with_type[idx+1][0]
+    next_stimulus_type=int(events_with_type[idx+1][2])
+        
     if stimulus_type not in timer.stimulusDict():
         # Allocate a new timer for this event type 
         # TODO: need protection if we run out of timers 
@@ -242,111 +276,29 @@ for idx, event in enumerate(events_with_type):
         timer.stimulusDict()[stimulus_type] = free_indices[:10].tolist()
         free_indices = free_indices[11:]
     
-    if event_type not in timer.eventDict():
-        # event type is really interval type (0-8) or A->B, B->A, etc
-        timer.eventDict()[event_type] = free_indices[:10].tolist()
-        free_indices = free_indices[11:]
-    
     ramps_stim_index = timer.stimulusDict()[stimulus_type]
     
-    # first event, e_0, activates < 200 ramps with s_1 = e_0 
-    
-    first_event=False   
-    event_time = events_with_type[idx][0] + .1
-    if idx<len(events_with_type)-1:
-            next_event = events_with_type[idx+1][0]
-    avg_weight = np.mean(timer.timers[ramps_stim_index])
-    timer_value = activationAtIntervalEnd(timer, ramps_stim_index, event_time - prev_event, NOISE)
-    next_event = 5
-    # plot house light indicator
-    # ax1.plot([0, events_with_type[idx][0]], [1.9, 1.9], 'k-', lw=4)
-    
     # All 200 ramps activate
-    ramps_stim_index = np.arange(0,len(timer.timers))
-    # timer activation from 0.1 to next event
-    timer_value = activationAtIntervalEnd(timer, ramps_stim_index, next_event, NOISE)
     
-    start_threshold_times = start_threshold_time(timer_value, event_time)
-    start_threshold_times.sort()
-    start_threshold_times = np.vstack((start_threshold_times, np.ones(len(start_threshold_times)))).T
+    timer_value = activationAtIntervalEnd(timer, ramps_stim_index, next_event - event_time, NOISE)    
+    for i in timer_value:
+        ax1.plot([event_time,next_event], [0, i], linestyle = "dashed",  c=colors[stimulus_type], alpha=0.5)
+        ax1.plot([next_event], [i], marker='o',c=colors[stimulus_type], alpha=0.2) 
     
-    stop_threshold_times = stop_threshold_time(timer_value, event_time)
-    stop_threshold_times.sort()
-    stop_threshold_times = np.vstack((stop_threshold_times, (-1* np.ones(len(stop_threshold_times))))).T
-    
-    start_stop_pairs = np.vstack((start_threshold_times, stop_threshold_times))
-    start_stop_pairs = start_stop_pairs[start_stop_pairs[:, 0].argsort()]
-    print(start_stop_pairs)
-    r = list(generate_responses(100))
-    r.insert(0, event_time)
-    r=list(np.cumsum(r))
-    
-    ri_start=True
-    plot_end=True
-
-    responses = []
-    k = 0
-    
-    
-    # Form list of start and stop events, sorted by time (a1, sig1, a2, a3, sig2, sig3 etc)
-    # Loop through all, if start event, k++, else, k--
-    # Identify all periods of k > K
-    # Fill with Poisson seq (samples then add the start time to all of them)
-    # once theyre greater than the boundary where they stop, throw them out
-    
-    for jdx, time in enumerate(start_stop_pairs):
-        k+=time[1]
-        print("k: ",k,"time: ", time[0])
-        # Generate all and cumsum instead of this
-        s =  r.pop(0) + event_time
-        if k >= K and s:
-            if ri_start:
-                rs=time
-                ri_start=False
-            
-            if s<next_event:    
-                responses.append(s)
-                #print(responses)
-                #print("next event: ",next_event)
-        #(k_count >= K and r and s<stop_threshold_times[-1]) and responses.append(s)
-   
-    ax1.plot(responses, np.ones(len(responses)), 'x')
-    
-    
-    
-    
-    # for t in range(start_stop_pairs[0][0], start_stop_pairs[-1][1], dt):
-        
-    #print(np.argwhere(start_stop_pairs > start_stop_pairs[0][1]))
-        
+    responses = respond(timer_value, event_time, next_event, ax1, idx)
     free_timers_vals = activationAtIntervalEnd(timer, free_indices, next_event, NOISE)
-    
-    response_time = generate_hit_time(timer.timerWeight(ramps_stim_index[0]), TIMER_THRESHOLD, NOISE, dt)
-    
-    coin_flip_update_rule(timer_value, timer, ramps_stim_index, prev_event, event_time, stimulus_type, event_type, next_stimulus_type, plot= False)    
-   
-    # Do we want to score the first event which we know is bad?
-    #timer.setScore(ramps_stim_index, timer.getScore(ramps_stim_index[0]) + score_decay(response_time, event_time))
-    ax1.vlines(event_time, 0,Y_LIM, label="v", color=colors[4 + int(event[2])])
-    #ax1.text(event_time,2.1,ALPHABET_ARR[int(events_with_type[idx][2])])
-    #ax1.text(event[0],2.1,ALPHABET_ARR[int(events_with_type[idx+1][2])])
-  
+    coin_flip_update_rule(timer_value, timer, ramps_stim_index, event_time, next_event, stimulus_type, event_type, next_stimulus_type, plot= False)
     
     
-    #for i, value in enumerate(timer_value):
-       #ax1.plot([start_threshold_times[i]], [START_THRESHOLD], marker='x', alpha=0.8) 
-       #ax1.plot([stop_threshold_times[i]], [STOP_THRESHOLD], marker='o', alpha=0.8) 
-      # ax1.plot([0,next_event], [0, value], linestyle = "dashed", c=colors[stimulus_type], alpha=0.1)
-       #plt.plot([event_time], [i], marker='o',c=colors[event_type],  alpha=0.2) 
-       # ax1.plot([response_time], [RESPONSE_THRESHOLD], marker='o', c=colors[stimulus_type], alpha=0.8) 
-       
     if PLOT_FREE_TIMERS:
         for i in free_timers_vals:
             ax1.plot([0,event_time], [0, i], linestyle = "dashed", c='grey', alpha=0.5)
+    ax1.vlines(event_time, 0,Y_LIM, label="v", color=colors[4 + int(event[2])])
     
-    ax1.set_ylim([0,Y_LIM])
-    ax1.set_xlim([0,event_time])
-    ax1.set_ylabel("Activation")
-    ax1.set_xlabel("Time")
-    ax1.grid('on')
+
+ax1.set_ylim([0,Y_LIM])
+ax1.set_xlim([0,T])
+ax1.set_ylabel("Activation")
+ax1.set_xlabel("Time")
+ax1.grid('on')
                 
